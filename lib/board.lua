48f47957ec2ec6e3d7b0ca4542a24b7fccaced41
Cymen Vig <cymenvig@gmail.com>
2012-06-26 14:26:29 -0500

    More work on board

--- lib/board.lua
+++ lib/board.lua
@@ -1,8 +1,24 @@
 Board = {}
 Board.__index = Board
 
-function Board.create()
+function Board.create(cells)
   local board = {}
   setmetatable(board, Board)
+  board.cells = cells
+  if( board.cells == nil )
+    then board.cells = {}
+  end
   return board
+end
+
+function Board:get(index)
+  return self.cells[index]
+end
+
+function Board:set(index, value)
+  table.insert(self.cells, index, value)
+end
+
+function Board:unset(index)
+  self:set(index, nil)
 end
