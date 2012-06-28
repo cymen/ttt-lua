d8249fc758c80c1ae9d19979865bbadad676c0d6
Cymen Vig <cymenvig@gmail.com>
2012-06-28 00:01:21 -0500

    Add to Output write_line_centered()

--- lib/output.lua
+++ lib/output.lua
@@ -1,10 +1,14 @@
 Output = {}
 Output.__index = Output
 
-function Output.create(buffer)
+function Output.create(buffer, width)
   local output = {}
   setmetatable(output, Output)
   output.buffer = buffer
+  if width == nil then
+    width = 80
+  end
+  output.width = width
   return output
 end
 
@@ -16,4 +20,16 @@ function Output:write(...)
       io.write(value)
     end
   end
+end
+
+function Output:write_line_centered(value, width)
+  if width == nil then
+    width = self.width
+  end
+
+  local length, padding
+  length = #(value)
+  padding = (width - length) / 2
+
+  self:write(string.rep(" ", padding) .. value, "\n")
 end
--- spec/output_spec.lua
+++ spec/output_spec.lua
@@ -1,14 +1,45 @@
 require "lib/output"
 
 describe("Output", function()
+  local buffer, output
+
+  before(function()
+    buffer = {}
+    output = Output.create(buffer)
+  end)
 
   context("write", function()
     it("writes to injected buffer", function()
-      local buffer = {}
-      local output = Output.create(buffer)
-
       output:write("Would you like to be x or o (x always goes first)? ")
-      assert_equal(buffer[#(buffer)], "Would you like to be x or o (x always goes first)? ")
+
+      assert_equal(buffer[1], "Would you like to be x or o (x always goes first)? ")
+    end)
+  end)
+
+  context("write_line_centered", function()
+    it("centers line of text", function()
+      output:write_line_centered("abcd", 20)
+
+      assert_equal(buffer[1], "        abcd")
+      assert_equal(buffer[2], "\n")
+    end)
+
+    it("prints without padding if string is longer than width", function()
+      output:write_line_centered("abcd", 2)
+
+      assert_equal(buffer[1], "abcd")
+    end)
+
+    it("default to width of 80 if none provided", function()
+      output:write_line_centered("abcd")
+
+      assert_equal(buffer[1], "                                      abcd")
+    end)
+
+    it("on odd length lines it 'floats' to the left by one column off center", function()
+      output:write_line_centered("abc", 8)
+      
+      assert_equal(buffer[1], "  abc")
     end)
   end)
 
