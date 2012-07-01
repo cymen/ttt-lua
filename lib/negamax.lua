32e81932d9018e91060edd3708ab7a0265ffa5f3
Cymen Vig <cymenvig@gmail.com>
2012-07-01 07:19:02 -0500

    Implemented Negamax

--- lib/negamax.lua
+++ lib/negamax.lua
@@ -1,4 +1,4 @@
-require "lib/player_next_empty"
+local Scorer = require "/lib/scorer"
 
 Negamax = {}
 Negamax.__index = Negamax
@@ -6,11 +6,89 @@ Negamax.__index = Negamax
 function Negamax.create()
   local negamax = {}
   setmetatable(negamax, Negamax)
-  negamax.infinity = math.huge
   return negamax
 end
 
 function Negamax:run(board)
-  local pne = PlayerNextEmpty.create()
-  return pne:play(board)
+  self.initial_player = Scorer.turn(board)
+  self.used_count_at_start = board:used_count()
+  local weighted_choices = {}
+
+  for _, cell_number in pairs(board:empty_cell_numbers()) do
+    board:set(cell_number, Scorer.turn(board))
+    weighted_choices[cell_number] = -self:negamax(board, 1, -math.huge, math.huge, -1)
+    board:clear(cell_number)
+  end
+
+  for key, value in pairs(weighted_choices) do
+    io.write(key .. " -> " .. value .. "\n")
+  end
+
+  return weighted_choices
+end
+
+function Negamax:negamax(board, depth, alpha, beta, sign)
+  if depth > 8 then
+    return alpha
+  end
+
+  if Scorer.is_over(board) then
+    return sign * self:analysis(board, depth)
+  end
+
+  local max = -math.huge
+
+  local choices = board:empty_cell_numbers()
+  self:sort_choices_optimally(choices)
+  for _, cell_number in pairs(choices) do
+    board:set(cell_number, Scorer.turn(board))
+    max = math.max(-self:negamax(board, depth + 1, -beta, -alpha, -sign), max)
+    board:clear(cell_number)
+    alpha = math.max(alpha, max)
+    if alpha >= beta then
+      return alpha
+    end
+  end
+
+  return max
+end
+
+function Negamax:analysis(board, depth)
+  if Scorer.is_won(board) then
+    if Scorer.winner(board) == self.initial_player then
+      return math.pow(2, -depth)
+    else
+      return -math.pow(2, -depth)
+    end
+  elseif Scorer.is_tie(board) then
+    return 0
+  end
+end
+
+function Negamax:is_optimal(cell_number)
+  if cell_number ~= nil and cell_number % 2 == 1 then
+    return true
+  else
+    return false
+  end
+end
+
+function Negamax:compare_optimal(a, b) --a_less_optimal_than_b(a, b)
+  if self:is_optimal(a) and self:is_optimal(b) then
+    return a < b
+  elseif self:is_optimal(a) then
+    return true
+  elseif self:is_optimal(b) then
+    return false
+  else
+    return a < b
+  end
+end
+
+function Negamax:sort_choices_optimally(choices)
+  function compare(a, b)
+    return self:compare_optimal(a, b)
+  end
+
+  table.sort(choices, compare)
 end
--- spec/negamax_spec.lua
+++ spec/negamax_spec.lua
@@ -1,4 +1,5 @@
 require "lib/negamax"
+require "lib/board"
 
 describe("Negamax", function()
   local negamax
@@ -8,10 +9,84 @@ describe("Negamax", function()
   end)
 
   context("create", function()
-    it ("can be created", function()
+    it("can be created", function()
       assert_not_equal(negamax, nil)
     end)
   end)
 
+  context("is_optimal", function()
+    it("odd numbers are optimal", function()
+      for i = 1,9,2 do
+        assert_true(negamax:is_optimal(i))
+      end
+    end)
+    it("even numbers are not optimal", function()
+      for i = 2,8,2 do
+        assert_false(negamax:is_optimal(i))
+      end
+    end)
+  end)
+
+  context("compare_optimal", function()
+    it("compares an odd number to an even number", function()
+      assert_true(negamax:compare_optimal(1, 4))
+      assert_true(negamax:compare_optimal(7, 2))
+    end)
+    it("compares an even number to an odd number", function()
+      assert_false(negamax:compare_optimal(8, 1))
+      assert_false(negamax:compare_optimal(4, 7))
+    end)
+    it("compares two odd numbers", function()
+      assert_true(negamax:compare_optimal(1, 3))
+      assert_false(negamax:compare_optimal(3, 1))
+    end)
+    it("compares two even numbers", function()
+      assert_true(negamax:compare_optimal(2, 4))
+      assert_false(negamax:compare_optimal(8, 4))
+    end)
+  end)
+
+  context("sort_choices_optimally", function()
+    it("sorts a list of cell numbers so that the known optimal (odd) are first", function()
+      local choices = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+      negamax:sort_choices_optimally(choices)
+
+      for i = 1,9 do
+        if i < 6 then
+          assert_true(choices[i] % 2 == 1)
+        else
+          assert_true(choices[i] % 2 == 0)
+        end
+      end
+    end)
+  end)
+
+  context("run", function()
+    it("weights the odd cells as equally optimal on an empty board", function()
+      local board = Board.create()
+      local weighted_choices = negamax:run(board)
+
+      assert_have_optimal_moves_of(weighted_choices, {1, 3, 5, 7, 9})
+    end)
+
+    it("blocks a winning move of the opponent", function()
+      local board = Board.create({ 'x', nil, 'x',
+                                   nil, nil, nil,
+                                   nil, nil, 'o' })
+      local weighted_choices = negamax:run(board)
+
+      assert_have_optimal_move_of(weighted_choices, 2)
+    end)
+  end)
+
+  context("analysis", function()
+    it("analyzes a tied game", function()
+      local board = Board.create({ 'x', 'o', 'x',
+                                   'o', 'x', 'o',
+                                   'o', 'x', 'o' })
+
+      assert_equal(negamax:analysis(board, 1), 0)
+    end)
+  end)
 
 end)
