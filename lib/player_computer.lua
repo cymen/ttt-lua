9a30411d53997bf9c56f5e21844508b7e94ee712
Cymen Vig <cymenvig@gmail.com>
2012-07-01 07:18:50 -0500

    Implemented PlayerComputer

--- lib/player_computer.lua
+++ lib/player_computer.lua
@@ -6,10 +6,56 @@ PlayerComputer.__index = PlayerComputer
 function PlayerComputer:create()
   local pc = {}
   setmetatable(pc, PlayerComputer)
+  pc.optimal = {1, 3, 5, 7, 9}
+  pc:prepare_random_value_generator()
   return pc
 end
 
 function PlayerComputer:play(board)
-  local negamax = Negamax.create()
-  return negamax:run(board)
+  local best_choices
+
+  if board:is_empty() then
+    best_choices = self.optimal
+  else
+    local negamax = Negamax.create()
+    local choices = negamax:run(board)
+    best_choices = self:best_choices(choices)
+  end
+
+  return self:random_value_from_table(best_choices)
+end
+
+function PlayerComputer:max_value_from_table(choices)
+  local max = -math.huge
+  for _, value in pairs(choices) do
+    if value > max then
+      max = value
+    end
+  end
+
+  return max
+end
+
+function PlayerComputer:best_choices(choices)
+  local best_choices = {}
+  local best_weight = self:max_value_from_table(choices)
+
+  for cell_number, weight in pairs(choices) do
+    if weight == best_weight then
+      table.insert(best_choices, cell_number)
+    end
+  end
+
+  return best_choices
+end
+
+function PlayerComputer:random_value_from_table(choices)
+  return choices[math.random(#(choices))]
+end
+
+function PlayerComputer:prepare_random_value_generator()
+  math.randomseed(os.time())
+  for i=1,20 do
+    math.random()
+  end
 end
--- spec/player_computer_spec.lua
+++ spec/player_computer_spec.lua
@@ -24,4 +24,40 @@ describe("PlayerComputer", function()
     end)
   end)
 
+  context("max_value_from_table", function()
+    it("returns the largest value present in the table", function()
+      local choices = {}
+      choices[1] = 100
+      choices[2] = 50
+      choices[3] = 25
+      choices[4] = 100
+
+      assert_equal(pc:max_value_from_table(choices), 100)
+    end)
+  end)
+
+  context("best_choices", function()
+    it("filters the choices of a table that is keyed by weight to the best", function()
+      local choices = {}
+      choices[1] = 100
+      choices[2] = 50
+      choices[3] = 25
+      choices[4] = 100
+
+      local result = pc:best_choices(choices)
+      assert_equal(#(result), 2)
+      assert_equal(result[1], 1)
+      assert_equal(result[2], 4)
+    end)
+  end)
+
+  context("random_value_from_table", function()
+    it("chooses on of the values from a table", function()
+      local choices = { 1, 5, 9 }
+
+      local result = pc:random_value_from_table(choices)
+      assert_true(result == 1 or result == 5 or result == 9)
+    end)
+  end)
+
 end)
